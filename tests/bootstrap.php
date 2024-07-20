73599c468cd5a88ccb76490fe966a24efeee479c
carlhandy <carl@kalpa.dev>
2024-07-20 02:51:56 -0400

    attempt to get path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,12 @@
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// Give access to tests_add_filter() function.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+// Load the WordPress test environment
+$wordpress_tests_path = getenv('WP_TESTS_PATH');
+if (!$wordpress_tests_path) {
+    die('WP_TESTS_PATH environment variable is not set.');
+}
+require_once $wordpress_tests_path . '/includes/functions.php';
 
 // Start up the WP testing environment.
 require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
