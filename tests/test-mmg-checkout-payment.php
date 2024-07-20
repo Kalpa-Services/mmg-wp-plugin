7c17b5fe002d7dc60aa017ad160ea8305f0a83dc
carlhandy <carl@kalpa.dev>
2024-07-20 08:27:39 -0400

    update rsa

--- tests/test-mmg-checkout-payment.php
+++ tests/test-mmg-checkout-payment.php
@@ -14,8 +14,15 @@ class Test_MMG_Checkout_Payment extends WP_UnitTestCase {
         update_option('mmg_secret_key', 'test_secret_key');
         update_option('mmg_merchant_id', 'test_merchant_id');
         update_option('mmg_client_id', 'test_client_id');
-        update_option('mmg_rsa_public_key', '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy8Dbv8prpJ/0kKhlGeJY\nozo2t60EG8L0561g13R29LvMR5hyvGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+\nvw1HocOAZtWK0z3r26uA8kQYOKX9Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQAp\nfc9jB9nTzphOgM4JiEYvlV8FLhg9yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68\ni6T4nNq7NWC+UNVjQHxNQMQMzU6lWCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoV\nPpY72+eVthKzpMeyHkBn7ciumk5qgLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUy\nwQIDAQAB\n-----END PUBLIC KEY-----');
-
+        update_option('mmg_rsa_public_key', '-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy8Dbv8prpJ/0kKhlGeJY
+    ozo2t60EG8L0561g13R29LvMR5hyvGZlGJpmn65+A4xHXInJYiPuKzrKUnApeLZ+
+    vw1HocOAZtWK0z3r26uA8kQYOKX9Qt/DbCdvsF9wF8gRK0ptx9M6R13NvBxvVQAp
+    fc9jB9nTzphOgM4JiEYvlV8FLhg9yZovMYd6Wwf3aoXK891VQxTr/kQYoq1Yp+68
+    i6T4nNq7NWC+UNVjQHxNQMQMzU6lWCX8zyg3yH88OAQkUXIXKfQ+NkvYQ1cxaMoV
+    PpY72+eVthKzpMeyHkBn7ciumk5qgLTEJAfWZpe4f4eFZj/Rc8Y8Jj2IS5kVPjUy
+    wQIDAQAB
+    -----END PUBLIC KEY-----');
         // Create a test order
         $order = wc_create_order();
         $order->set_total(100);
