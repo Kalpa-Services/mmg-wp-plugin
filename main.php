f242d9a373aeb2b9a53cabbe75904d862bc72de8
Carl Handy <carl@kalpa.dev>
2024-07-21 15:54:40 +0000

    Bump version to 1.1.14

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: MMG Checkout Payment
 Description: Enables MMG Checkout Payment flow for registered MMG Merchants to receive E-Commerce payments from MMG customers.
-Version: 1.1.13
+Version: 1.1.14
 Author: Kalpa Services Inc.
 */
 
