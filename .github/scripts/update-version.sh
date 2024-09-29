e1861c4fae2897fe73cbb4a59fab4383552990e6
Carl Handy <carl@kalpa.dev>
2024-09-29 01:36:18 -0400

    remove missing file

--- .github/scripts/update-version.sh
+++ .github/scripts/update-version.sh
@@ -12,7 +12,7 @@ sed -i "s/\(define('MMG_PLUGIN_VERSION', '\)[0-9.]\+/\1$NEW_VERSION/" main.php
 # Update the Stable tag in README.txt
 sed -i "s/\(Stable tag: \)[0-9.]\+/\1$NEW_VERSION/" README.txt
 
-git add README.txt updater.json main.php
+git add README.txt main.php
 git commit -m "Bump version to $NEW_VERSION"
 
 # Push the change to the main branch
